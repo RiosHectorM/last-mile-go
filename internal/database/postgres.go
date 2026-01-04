308e5bda050c60cf3b318218d006e1e88b4ae96d
RiosHectorM <rios.hector.martin@gmail.com>
2026-01-03 23:48:56 -0300

    Implementar variables de entorno y mejorar seguridad

--- internal/database/postgres.go
+++ internal/database/postgres.go
@@ -3,24 +3,25 @@ package database
 import (
 	"database/sql"
 	"fmt"
-	_ "github.com/lib/pq" // El guion bajo es porque solo necesitamos el driver
+	"os"
+
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
 
 func GetConnection() (*sql.DB, error) {
-	// Estos datos deben coincidir con tu docker-compose.yml
-	connStr := "host=localhost port=5432 user=user_logistics password=password123 dbname=last_mile_db sslmode=disable"
+	// Cargamos el archivo .env
+	godotenv.Load()
 
-	db, err := sql.Open("postgres", connStr)
-	if err != nil {
-		return nil, err
-	}
+	// Leemos las variables
+	user := os.Getenv("DB_USER")
+	pass := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
 
-	// Verificar si la conexión es real
-	err = db.Ping()
-	if err != nil {
-		return nil, err
-	}
+	connStr := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		host, port, user, pass, dbName)
 
-	fmt.Println("✅ Conexión a la base de datos exitosa!")
-	return db, nil
+	return sql.Open("postgres", connStr)
 }
